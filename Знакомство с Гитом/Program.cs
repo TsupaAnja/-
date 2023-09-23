f9d24a9bbca75175b54ee413ca8eb4e4d23afd9a
TsupaAnja <tsupaanja2015@gmail.com>
2023-09-24 02:15:12 +0300

    Changed the inscription

--- Знакомство с Гитом/Program.cs
+++ Знакомство с Гитом/Program.cs
@@ -8,7 +8,7 @@ namespace Знакомство_с_Гитом
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Привет, Гит!");
-            Console.WriteLine("Осваиваю Гит");
+            Console.WriteLine("Осваиваю Гит и SourceTree");
         }
     }
 }
