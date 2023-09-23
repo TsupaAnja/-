f2485f1c5367b3f0339af4f461a4ff6c9b7f3e54
HP <HP@DESKTOP-O72OG72>
2023-09-23 17:15:26 +0300

    Добавьте файлы проекта.

---
+++ Знакомство с Гитом/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Знакомство_с_Гитом
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
